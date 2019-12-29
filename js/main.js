9a1f44798010b0a5fb7fe290428403d68ae85816
Kirnukan <serjant.saygon@gmail.com>
2019-12-29 14:52:06 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const mobileWidth = 320;
+const miniDesktopWidth = 768;
+const desktopWidth = 1120;
+var swiperList = document.querySelector('.swiper-container').swiper;
+window.addEventListener('resize', function(){
+if(window.innerWidth >= miniDesktopWidth){
+var swiperList =  new Swiper('.swiper-container',{
+    slidesPerView: 'auto',
+    slidesOffsetBefore: 16,
+    slidesOffsetAfter: 16,
+    spaceBetween: 16,
+    watchOverflow: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    }
+  });
+} 
+});
