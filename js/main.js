4e47b12c842249ed87ea44d57a6381ed77e487da
Kirnukan <serjant.saygon@gmail.com>
2019-12-30 17:43:46 +0500

    1

--- js/main.js
+++ js/main.js
@@ -1,18 +1,29 @@
 const mobileWidth = 320;
 const miniDesktopWidth = 768;
 const desktopWidth = 1120;
-var swiperList = document.querySelector('.swiper-container').swiper;
-
-
-var swiperList =  new Swiper('.swiper-container',{
+//var swiperList = document.querySelector('.swiper-container').swiper;
+let swiperValues = {
+  
     slidesPerView: 'auto',
     slidesOffsetBefore: 16,
     slidesOffsetAfter: 16,
     spaceBetween: 16,
-    watchOverflow: false,
     pagination: {
       el: '.swiper-pagination',
       clickable: true
     }
-  });
+
+  
+};
+if(window.innerWidth < miniDesktopWidth){
+ swiperList =  new Swiper('.swiper-container', swiperValues);
+}
+const swiperFunc = function(evt){
+  if (window.innerWidth >= miniDesktopWidth) {
+    swiperList.destroy();
+  } else {
+    swiperList = new Swiper('.swiper-container', swiperValues)
+  }
+}
+  window.addEventListener('resize', swiperFunc);
 
