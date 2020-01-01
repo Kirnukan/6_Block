caf0f7cafdb4397370797b1a2364212fc1f8bb20
Kirnukan <serjant.saygon@gmail.com>
2020-01-01 13:55:04 +0500

    1

--- js/main.js
+++ js/main.js
@@ -27,3 +27,23 @@ const swiperFunc = function(evt){
 }
   window.addEventListener('resize', swiperFunc);
 
+let swiperBtn = document.querySelector('.slider-section__btn');
+swiperBtn.addEventListener('click', function(evt){
+  let slides = document.querySelectorAll('.swiper__item'); 
+  let sliderSection = document.querySelector('.slider-section');
+  for(let i = 6; i <= slides.length; i++){
+    if(slides[i].style.display == 'none'){
+      slides[i].style.display = 'flex';
+    } else {
+      slides[i].style.display = 'none';
+    }
+    if(sliderSection.style.maxHeight == '322px' && sliderSection.style.height == '322px'){
+      sliderSection.style.maxHeight = '490px';
+      sliderSection.style.height = '490px';
+    } else {
+      sliderSection.style.maxHeight = '322px';
+      sliderSection.style.height = '322px';
+    }
+  }
+  
+});
